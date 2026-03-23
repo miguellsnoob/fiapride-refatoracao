39ef2ef682c21a65e359cf87ba756f2409b8045b
miguel <emc.miguellima@gmail.com>
2026-03-23 19:21:03 -0300

    Add Principal class with main method for Veiculo

---
+++ principal.java
@@ -0,0 +1,18 @@
+package br.com.fiapride.main;
+
+import br.com.fiapride.model.Veiculo;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Veiculo v1 = new Veiculo("Carlos", "ABC-1234", 0);
+
+        v1.abastecer(50);
+        v1.consumir(30);
+
+        System.out.println("Dono: " + v1.getDono() +
+                " | Placa: " + v1.getPlaca() +
+                " | Combustível: " + v1.getCombustivel());
+    }
+}
